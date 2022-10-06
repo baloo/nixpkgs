e376842a46291418b963353229a8da6dd5cac09b
Ruud van Asseldonk <dev@veniogames.com>
2022-10-06 22:07:02 +0200

    libressl: 3.5.3 -> 3.6.0
    We should also remove 3.4, as it will become unsupported in a week (one
    year after the OpenBSD 7.0 release), but some packages explicitly depend
    on it, so I'm not removing it yet.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19984,9 +19984,10 @@ with pkgs;
 
   inherit (callPackages ../development/libraries/libressl { })
     libressl_3_4
-    libressl_3_5;
+    libressl_3_5
+    libressl_3_6;
 
-  libressl = libressl_3_4;
+  libressl = libressl_3_6;
 
   boringssl = callPackage ../development/libraries/boringssl { };
 
