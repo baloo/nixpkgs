254ccb262da08c3016401250aaacd43c865f6020
D Anzorge <d.anzorge@gmail.com>
2022-12-13 02:47:50 +0100

    nixos/grafana: allow @chown syscalls when using unix sockets
    Grafana will unconditionally call chown on the socket after creating it,
    even if the configuration does not ask for a different socket gid.

--- nixos/modules/services/monitoring/grafana.nix
+++ nixos/modules/services/monitoring/grafana.nix
@@ -707,7 +707,10 @@ in {
         SystemCallArchitectures = "native";
         # Upstream grafana is not setting SystemCallFilter for compatibility
         # reasons, see https://github.com/grafana/grafana/pull/40176
-        SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" ];
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged"
+        ] ++ lib.optional (cfg.protocol == "socket") [ "@chown" ];
         UMask = "0027";
       };
       preStart = ''
