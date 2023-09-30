0a04c86002a7c135fd090f7ff515362bb21195ce
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-09-30 01:34:32 -0700

    python3Packages.greynoise: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3721,6 +3721,8 @@ in {
 
   gremlinpython = callPackage ../development/python-modules/gremlinpython { };
 
+  greynoise = callPackage ../development/python-modules/greynoise { };
+
   growattserver = callPackage ../development/python-modules/growattserver { };
 
   gridnet = callPackage ../development/python-modules/gridnet { };
