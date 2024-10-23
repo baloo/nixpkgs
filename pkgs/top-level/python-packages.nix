44aed75424b2cd00047048717c477380f02ef006
Arthur Gautier <arthur.gautier@arista.com>
2024-10-22 21:27:19 -0700

    ruff: move to buildPythonPackage

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13901,6 +13901,10 @@ self: super: with self; {
 
   rubymarshal = callPackage ../development/python-modules/rubymarshal { };
 
+  ruff = toPythonModule (pkgs.ruff.override {
+    python3Packages = self;
+  });
+
   ruff-api = callPackage ../development/python-modules/ruff-api { };
 
   ruffus = callPackage ../development/python-modules/ruffus { };
